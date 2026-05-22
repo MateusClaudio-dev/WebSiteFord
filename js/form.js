3c734bb565b64a23daf59169fe154f37cf9a6cd6
MATEUS CLAUDIO ALVES DA SILVA <mateusclaudio.dev@gmail.com>
2026-05-22 16:56:31 -0300

    carrossel

--- js/form.js
+++ js/form.js
@@ -2,12 +2,23 @@
 //class contato
 
 class contato {
-    
+
+
+constructor(nome, sobrenome, email, cpf, telefone, meioContato) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.email = email;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.meioContato = meioContato;
+    }
+
 }
 
 function Post(form) {
 
-  let data = new contato(form.elements.namedItem("nome").value,
+  let data = new contato(
+            form.elements.namedItem("nome").value,
             form.elements.namedItem("sobrenome").value, 
             form.elements.namedItem("email").value, 
             form.elements.namedItem("cpf").value, 
